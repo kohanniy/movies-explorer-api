d92fac2754e6fdd9f69504b77e6c1a678d990a1b
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-05-05 09:46:32 +0500

    создать схемы и модели user и movie

---
+++ models/movie.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const { validationErrorMsg } = require('../utils/errorMessages');
+const { urlValidator } = require('../utils/validators');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+  },
+  director: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+  },
+  duration: {
+    type: Number,
+    required: [true, validationErrorMsg.requiredField],
+  },
+  year: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+  },
+  description: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+  },
+  image: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+    validate: {
+      validator: urlValidator,
+      message: validationErrorMsg.invalidURL,
+    },
+  },
+  trailer: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+    validate: {
+      validator: urlValidator,
+      message: validationErrorMsg.invalidURL,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+    validate: {
+      validator: urlValidator,
+      message: validationErrorMsg.invalidURL,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+  },
+  nameEN: {
+    type: String,
+    required: [true, validationErrorMsg.requiredField],
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+const regexToCheckURL = /https?:\/\/[www.]?[0-9a-z-]{2,}\.[a-z0-9]{2,6}[a-z0-9-._~:/?#[\]@!$&'()*+,;=]*/;
+
+module.exports = {
+  regexToCheckURL,
+};
