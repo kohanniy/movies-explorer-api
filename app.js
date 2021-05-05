81ccc77324ab7f5a70753743a202e170386ad492
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-05-05 11:21:58 +0500

    создать роуты

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/index');
+const NotFoundError = require('./errors/NotFound');
+const requestErrors = require('./utils/errorMessages');
 
 const app = express();
 
@@ -11,6 +16,15 @@ mongoose.connect('mongodb://localhost:27017/filmsdb', {
 });
 
 const { PORT = 3000 } = process.env;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.all('/*', () => {
+  throw new NotFoundError(requestErrors.notFound.url);
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
