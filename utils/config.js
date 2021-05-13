d7f1e66a1896f4bacbd61b4dcb7ad727a0c5ca71
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-05-13 14:02:05 +0500

    создать конфигурационный файл для сервера

---
+++ utils/config.js
@@ -0,0 +1,20 @@
+const {
+  NODE_ENV,
+  JWT_SECRET,
+  MONGO_URL,
+  PORT = 3000,
+} = process.env;
+
+const mongooseParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
+module.exports = {
+  JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+  MONGO_URL: NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb',
+  PORT,
+  mongooseParams,
+};
