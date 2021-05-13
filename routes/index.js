d229814eedd4344200685c550cccbdfa4ebd7602
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-05-13 17:41:47 +0500

    защитить маршрут по умолчанию авторизацией

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const moviesRouter = require('./movies');
 const usersRouter = require('./users');
+const NotFoundError = require('../errors/NotFound');
+const { requestErrors } = require('../utils/errorMessages');
 const { createUser, login } = require('../conrollers/users');
 const { checkNewUser, checkLogin } = require('../middlewares/checkData');
 const auth = require('../middlewares/auth');
@@ -8,7 +10,11 @@ const auth = require('../middlewares/auth');
 router.post('/signup', checkNewUser, createUser);
 router.post('/signin', checkLogin, login);
 
-router.use('/movies', auth, moviesRouter);
-router.use('/users', auth, usersRouter);
+router.use(auth);
+router.use('/movies', moviesRouter);
+router.use('/users', usersRouter);
+router.all('*', () => {
+  throw new NotFoundError(requestErrors.notFound.url);
+});
 
 module.exports = router;
